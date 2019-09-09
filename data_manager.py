7fbe5d174c504f4989669bc5c4d8a768908815e5
LaszloNagy85 <n.laszlo.uk@gmail.com>
2019-09-09 12:09:03 +0200

    initial

--- data_manager.py
+++ data_manager.py
@@ -10,3 +10,91 @@ def get_mentor_names_by_first_name(cursor, first_name):
                    {'first_name': first_name})
     names = cursor.fetchall()
     return names
+
+
+@database_common.connection_handler
+def get_first_last_names(cursor):
+    cursor.execute("SELECT first_name, last_name FROM mentors;")
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def get_miskolc_nicks(cursor, location):
+    cursor.execute("""
+                    SELECT nick_name FROM mentors
+                    WHERE city = %(location)s;
+                    """, {'location': location})
+    nicks = cursor.fetchall()
+    print(nicks)
+    return nicks
+
+
+@database_common.connection_handler
+def get_carol_applicant_data(cursor, first_name):
+    cursor.execute("""
+                    SELECT first_name, last_name, phone_number FROM applicants
+                    WHERE first_name = %(first_name)s;
+                    """,{'first_name': first_name})
+    carol_data = cursor.fetchall()
+    return carol_data
+
+
+@database_common.connection_handler
+def get_hat_owner_applicant_data(cursor, email):
+    cursor.execute("""
+                    SELECT first_name, last_name, phone_number FROM applicants
+                    WHERE email LIKE %(email)s;
+                    """, {'email': email})
+    hat_owners_data = cursor.fetchall()
+    return hat_owners_data
+
+
+@database_common.connection_handler
+def insert_new_applicant(cursor, first_name, last_name, phone_number, email, application_code):
+
+    # cursor.execute("""
+    #                 INSERT INTO applicants (first_name, last_name, phone_number, email, application_code)
+    #                 VALUES (%(first_name)s, %(last_name)s, %(phone_number)s, %(email)s, %(application_code)s);
+    #                 """,
+    #                {'first_name': first_name, 'last_name': last_name, 'phone_number': phone_number,
+    #                 'email': email, 'application_code': application_code})
+
+    cursor.execute("""
+                    SELECT * FROM applicants
+                    WHERE application_code = %(application_code)s;
+                    """,
+                   {'application_code': application_code})
+    new_applicant_data = cursor.fetchall()
+    return  new_applicant_data
+
+
+@database_common.connection_handler
+def update_data(cursor, first_name, last_name, phone_number):
+    cursor.execute("""
+                    UPDATE applicants
+                    SET phone_number = %(phone_number)s
+                    WHERE first_name = %(first_name)s AND last_name = %(last_name)s
+                    """,
+                   {'first_name': first_name, 'last_name': last_name, 'phone_number': phone_number})
+    cursor.execute("""
+                    SELECT * FROM applicants
+                    WHERE first_name = %(first_name)s AND last_name = %(last_name)s
+                    """,
+                   {'first_name': first_name, 'last_name': last_name})
+    updated_data = cursor.fetchall()
+    return updated_data
+
+
+@database_common.connection_handler
+def delete_applicants_mauriseu(cursor, email_domain):
+    cursor.execute("""
+                    DELETE FROM applicants
+                    WHERE email LIKE %(email_domain)s;
+                    """,
+                   {'email_domain': email_domain})
+    cursor.execute("""
+                    SELECT * FROM applicants
+                    """)
+    remaining_applicants = cursor.fetchall()
+    return remaining_applicants
--- main.py
+++ main.py
@@ -11,12 +11,58 @@ def index():
 
 
 @app.route('/mentors-with-best-first-name')
-def mentor_names():
+def route_mentor_names():
     # We get back dictionaries here (for details check 'database_common.py')
     mentor_names = data_manager.get_mentor_names_by_first_name('Imre')
 
-    return render_template('mentor_names.html', mentor_names=mentor_names)
+    return render_template('mentor_names.html', data=mentor_names)
+
+
+@app.route('/mentors_first_last_names')
+def route_first_last_names():
+    names = data_manager.get_first_last_names()
+    return render_template('mentor_names.html', data=names)
+
+
+@app.route('/mentors_miskolc_nick_names')
+def route_miskolc_nick_names():
+    nicks = data_manager.get_miskolc_nicks('Miskolc')
+    return render_template('mentor_names.html', data=nicks)
+
+
+@app.route('/applicant_carol_data')
+def route_get_carol_applicant_data():
+    carol_data = data_manager.get_carol_applicant_data('Carol')[0]
+    return render_template("applicant_data.html", data=carol_data)
+
+
+@app.route('/applicant_hat_owners_data')
+def route_get_hat_owner_applicant_data():
+    hat_owner_data = data_manager.get_hat_owner_applicant_data('%@adipiscingenimmi.edu')[0]
+    return render_template('applicant_data.html', data=hat_owner_data)
+
+
+@app.route('/add_new_applicant')
+def route_add_new_applicant():
+    new_applicant_data = data_manager.insert_new_applicant('Markus', 'Schaffarzyk', '003620/725-2666',
+                                                           'djnovus@groovecoverage.com', '54823')
+    return render_template('mentor_names.html', data=new_applicant_data)
+
+
+@app.route('/update_phone_number')
+def route_update_and_show():
+    updated_data = data_manager.update_data('Jemima', 'Foreman', '003670/223-7459')
+    return render_template('mentor_names.html', data=updated_data)
+
+
+@app.route('/delete_applicants_from_mauriseu')
+def route_delete_and_show_all():
+    remaining_applicants = data_manager.delete_applicants_mauriseu('%mauriseu.net')
+    return render_template('mentor_names.html', data=remaining_applicants)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        port=8000,
+        debug=False,
+    )
